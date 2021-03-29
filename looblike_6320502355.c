833ee3f2e65f242b2681c24c028f555c31320b44
kulwatee <kulwatee.j@ku.th>
2021-03-29 17:27:25 +0700

    hj

--- looblike_6320502355.c
+++ looblike_6320502355.c
@@ -1,11 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n,max,i,j,k;
     scanf("%d",&n);
-    int x[n];
+    int x[n],y[10000];
+    for(j=1;j<=10000;j++)
+    {
+        y[j]=0;
+    }
     for(i=0;i<n;i++)
     {
         scanf("%d",&x[i]);
+        y[x[i]]++;
     }
+    max=y[1];
+    for(k=2;k<=10000;k++)
+    {
+        if(y[k]>max)
+        {
+            max=y[k];
+        }
+    }
+    printf("%d",max);
+
+
 }
