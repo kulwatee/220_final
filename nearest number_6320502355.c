407b0372de703f2a9f04195a5d854a5f93961f06
kulwatee <kulwatee.j@ku.th>
2021-03-29 18:46:09 +0700

    hui

--- nearest number_6320502355.c
+++ nearest number_6320502355.c
@@ -1,17 +1,35 @@
 #include<stdio.h>
 int main()
 {
-    int a,b;
+    int a,b,k=0,x,z;
     scanf("%d %d",&a,&b);
-    while()
+    x=a;
+    while(x>0)
+    {
+        k=x%10;
+        x=x/10;
+        if(k==b)
+            break;
+    }
+    if(k==b)
+        printf("%d",a);
+
+    else
     {
         x=a;
-        while(x>0)
+        z=x%10;
+        if(z>b)
         {
-           k=x%10;
-           x=x/10;
+          printf("%d",a+((b+10)-z));
+        }
+        else if(z<b)
+        {
+            printf("%d",a+(b-z));
         }
     }
+
+
+
 
 
 }
